f35419b23e9ceb7f545cda4a4959c598fe4f79fb
Egggggggggggg <infinitegamer090@gmail.com>
2026-01-30 19:45:52 +0530

    Physics and Raycast movement fusion

--- player/player_controller.gd
+++ player/player_controller.gd
@@ -1,33 +1,32 @@
 extends RigidBody3D
-
-@export var kick := 150.0
-@export var turn_speed :=5.0
-@export var brake_strength := 2.0
-@export var traction := 0.1
-@export var jump_force := 500.0
-@onready var facing_direction:= -transform.basis.z
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	linear_damp = traction
-	angular_damp = traction
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+@onready var rays = get_tree().get_nodes_in_group("Raycasts")
+@export var speed := 15000.0
+@export var turn_speed := 2000.0
+@export var reverse_speed := 1000.0
+@export var hover_force := 1000.0
+@export var jump_force := 100.0
+func _ready():
 	pass
-
+	
 func _physics_process(delta: float) -> void:
+	var can_jump = false
+	for ray in rays:
+		ray.force_raycast_update()
+		if ray.is_colliding():
+			can_jump=true
+			var collision_point = ray.get_collision_point()
+			
+			var distance = collision_point.distance_to(ray.global_position)
+			
+			apply_force(delta*(Vector3.UP*hover_force)/clamp(distance,0.1,10),ray.global_transform.origin - global_transform.origin)
+	
 	var turn = Input.get_axis("move_left","move_right")
 	if turn:
-		facing_direction=facing_direction.rotated(Vector3.UP,turn*turn_speed*delta)
+		apply_torque(-transform.basis.y*turn*turn_speed*delta)
 	
-	if Input.is_action_just_pressed("jump"):
-		apply_central_force(Vector3.UP*jump_force)
 	if Input.is_action_pressed("accelerate"):
-		apply_torque(facing_direction*kick)
+		apply_central_force(-transform.basis.z*speed*delta)
 	if Input.is_action_pressed("brake"):
-		linear_damp = brake_strength
-		angular_damp = brake_strength
-	else:
-		linear_damp = traction
-		angular_damp = traction
+		apply_central_force(transform.basis.z*reverse_speed*delta)
+	if Input.is_action_just_pressed("jump") and can_jump:
+		apply_impulse(transform.basis.y*jump_force)
