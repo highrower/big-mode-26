32b7d7403ebd5bd4856f9ba07bd376de80b6d06c
highrower <jairojared@gmail.com>
2026-01-31 07:59:33 -0600

    tinker with the rigidbody implementation, use integrateforces
    there is still more tinkering i wanna take with this approach but i feel like its different enough from eggyolks implementation that its worth branchning out

--- player/player_controller.gd
+++ player/player_controller.gd
@@ -1,32 +1,56 @@
 extends RigidBody3D
+
 @onready var rays = get_tree().get_nodes_in_group("Raycasts")
-@export var speed := 15000.0
-@export var turn_speed := 2000.0
-@export var reverse_speed := 1000.0
-@export var hover_force := 1000.0
-@export var jump_force := 100.0
+
+@export var acceleration := 50.0 
+@export var max_speed := 40.0
+@export var turn_speed := 2.5 # Radians per second
+@export var hover_force := 50.0 
+@export var jump_force := 20.0
+
+@export_range(0.0, 1.0) var drift_snappiness := 0.9 
+
 func _ready():
-	pass
+	# Optional: Prevents the board from flipping over entirely if you hit a weird bump
+	angular_damp = 3.0 
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var dt = state.step
 	
-func _physics_process(delta: float) -> void:
-	var can_jump = false
+	var is_grounded = false
 	for ray in rays:
 		ray.force_raycast_update()
 		if ray.is_colliding():
-			can_jump=true
+			is_grounded = true
 			var collision_point = ray.get_collision_point()
+			var dist = collision_point.distance_to(ray.global_position)
 			
-			var distance = collision_point.distance_to(ray.global_position)
+			var force_mag = (hover_force / clamp(dist, 0.1, 2.0))
+			var force_dir = state.transform.basis.y 
+			var pos_offset = ray.global_position - state.transform.origin
 			
-			apply_force(delta*(Vector3.UP*hover_force)/clamp(distance,0.1,10),ray.global_transform.origin - global_transform.origin)
+			state.apply_force(force_dir * force_mag, pos_offset)
+
+
+	var turn_input = Input.get_axis("move_right", "move_left") # Inverted for standard turning
+	var gas_input = Input.get_axis("brake", "accelerate")
 	
-	var turn = Input.get_axis("move_left","move_right")
-	if turn:
-		apply_torque(-transform.basis.y*turn*turn_speed*delta)
+
+	if turn_input != 0:
+		var rot_amount = turn_input * turn_speed
+		state.angular_velocity.y = (state.transform.basis.y * rot_amount).y
+		# Todo: Add some banking (roll) here for visuals
+
+	var local_velocity = state.transform.basis.inverse() * state.linear_velocity
 	
-	if Input.is_action_pressed("accelerate"):
-		apply_central_force(-transform.basis.z*speed*delta)
-	if Input.is_action_pressed("brake"):
-		apply_central_force(transform.basis.z*reverse_speed*delta)
-	if Input.is_action_just_pressed("jump") and can_jump:
-		apply_impulse(transform.basis.y*jump_force)
+	local_velocity.x = lerp(local_velocity.x, 0.0, drift_snappiness)
+	
+	if gas_input > 0 and local_velocity.z > -max_speed:
+		local_velocity.z -= acceleration * dt
+	elif gas_input < 0:
+		local_velocity.z += acceleration * dt
+		
+	state.linear_velocity = state.transform.basis * local_velocity
+
+	if Input.is_action_just_pressed("jump") and is_grounded:
+		state.apply_central_impulse(state.transform.basis.y * jump_force)
