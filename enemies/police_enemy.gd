13acd540c1074c3f298dbd9413e802a61f6650be
Egggggggggggg <infinitegamer090@gmail.com>
2026-02-07 15:14:52 +0530

    Fixed turn rotating not existing issue

--- enemies/police_enemy.gd
+++ enemies/police_enemy.gd
@@ -5,6 +5,7 @@ extends EnemyObject
 @onready var ChasePlayer = $ChasePlayer
 @onready var PatrolAround = $PatrolAround
 @onready var TurnRotating = $TurnRotating
+
 func _physics_process(delta: float) -> void:
 	if PlayerSight.target_visible:
 		ChasePlayer.speed = chase_speed
