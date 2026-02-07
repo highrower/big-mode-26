399d0b0fc9723a8fdd916e4bfc8e0d639a35c8b7
Egggggggggggg <infinitegamer090@gmail.com>
2026-02-07 14:52:54 +0530

    Fixed merge jank

--- enemies/behaviors/turn_rotating.gd
+++ enemies/behaviors/turn_rotating.gd
@@ -6,4 +6,4 @@ func execute(caller,_delta: float):
 	$Node3D.look_at(target)
 	for i in caller.get_children():
 		if i.is_in_group("rotating"):
-			i.rotation.y=$Node3D.rotation.y
+			i.rotation.y=$Node3D.rotation.y+PI
--- enemies/police_enemy.gd
+++ enemies/police_enemy.gd
@@ -4,15 +4,17 @@ extends EnemyObject
 @onready var PlayerSight = $PlayerSight
 @onready var ChasePlayer = $ChasePlayer
 @onready var PatrolAround = $PatrolAround
-
+@onready var TurnRotating = $TurnRotating
 func _physics_process(delta: float) -> void:
 	if PlayerSight.target_visible:
 		ChasePlayer.speed = chase_speed
 		var target = PlayerSight.last_raycast.get_collision_point()
+		TurnRotating.target=target
 		ChasePlayer.update_target(target)
 	else:
 		ChasePlayer.speed = patrol_speed
 		var target = PatrolAround.target
+		TurnRotating.target=target
 		ChasePlayer.update_target(target)
 	_behaviour_manager(delta)
 
